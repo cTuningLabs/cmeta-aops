816914bfbb100af3064bef3bf046fc6ea0f260dd
Grigori Fursin <gfursin@gmail.com>
2026-03-23 14:37:35 +0100

    forgot to add brew cMeta customization

---
+++ tool/brew/api_v1.py
@@ -0,0 +1,34 @@
+﻿import os
+
+from tool_c393ba5c6fa14f66.api.ctool import InitCTool
+
+class CTool(InitCTool):
+    """
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, module_file_path = __file__, **kwargs)
+
+
+    ############################################################
+    def customize_install_cmd(self,
+                              ctx: dict,
+                              install_cmd: str = None,
+                              params: dict = {},
+                              env: dict = {},
+                              timeout: int = None,
+                              *misc: dict,
+    ):
+        """
+        """
+
+        result = {'return':0}
+
+        passwordless_sudo = ctx['tasks']['global']['host'].get('os_extra', {}).get('passwordless_sudo', False)
+
+        if passwordless_sudo:
+            env['NONINTERACTIVE'] = 1
+
+        result['timeout'] = None
+
+        return result
