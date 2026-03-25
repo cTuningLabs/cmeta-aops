cdb6b21bacc63f91ee712cac514bf191cc67241a
Grigori Fursin <gfursin@gmail.com>
2026-03-25 15:38:27 +0100

    clean up

--- task/test-python-numpy/src/app.py
+++ task/test-python-numpy/src/app.py
@@ -1,8 +1,14 @@
 ﻿import sys
 import numpy as np
 
-print("Python version:", sys.version)
-print("Python executable path:", sys.executable)
-print("NumPy version:", np.__version__)
+sys.stdout.flush()
+
+print ('')
+print ('*'*40)
+print ("Python version:", sys.version)
+print ("Python executable path:", sys.executable)
+print ("NumPy version:", np.__version__)
+print ('*'*40)
+print ('')
 
 sys.stdout.flush()
