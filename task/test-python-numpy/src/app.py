51bbd8b8ef3c8f80a3377f354474577792f97891
Grigori Fursin <gfursin@gmail.com>
2026-03-25 15:44:59 +0100

    added extra_line for GitHub tests

--- task/test-python-numpy/src/app.py
+++ task/test-python-numpy/src/app.py
@@ -3,6 +3,10 @@ import numpy as np
 
 sys.stdout.flush()
 
+if '--extra_line=True' in sys.argv:
+    for _ in range(100):
+        print ('')
+
 print ('')
 print ('*'*40)
 print ("Python version:", sys.version)
