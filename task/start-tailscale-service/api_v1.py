df7a6d05130370956f23fdc7e883953ee0e632a9
Grigori Fursin <gfursin@gmail.com>
2026-03-29 15:53:03 +0200

    improved tailscale handling

---
+++ task/start-tailscale-service/api_v1.py
@@ -0,0 +1,62 @@
+﻿import os
+
+from task_c36be4b9314a45e0.api.ctask import InitCTask
+
+class CTask(InitCTask):
+    """
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, module_file_path = __file__, **kwargs)
+
+    ############################################################
+    def run(self,
+            ctx: dict,        # cMeta context
+            env: dict = {},
+    ):
+
+        """
+        Untar file.
+
+        Returns:
+            dict: A cMeta dictionary with the following keys:
+                - **return** (int): 0 if success, >0 if error.
+                - **error** (str): Error message if `return > 0`.
+        """
+
+        self.logger.debug("RUNNING TASK untar-file run api_v1")
+
+        con = ctx['control'].get('con', False)
+        quiet = ctx['control'].get('quiet', False)
+        verbose = ctx['control'].get('verbose', False)
+
+        space = '  ' * ctx['tasks']['nested_call'] if verbose else ''
+        clean = ctx['tasks']['run_control'].get('clean', False)
+        update = ctx['tasks']['run_control'].get('update', False)
+
+        uname = ctx['tasks']['global']['host']['os']['uname']
+
+        path_to_tailscale = ctx['tasks']['global']['tailscale']['qpath']
+
+        cmd = 'sudo brew services start tailscale'
+
+        ii = {'category': self.category_alias + ',' + self.category_uid,
+              'command': 'run',
+              'ctx': ctx,
+              'arg1': 'cmd,c9ba0a88df394d7f',
+              'cmd': cmd,
+              'env': env,
+              'con': con, 
+              'quiet': quiet,
+              'verbose': verbose, 
+              'quiet': quiet,
+              'text_cmd': 'RUN:',
+#              'print_env_keys': [], 
+              'print_extra_line': True,
+              'fail_if_nonzero_return_code': True,
+        }
+
+        rx = self.cm.access(ii)
+        if self.cm.catch_error(rx): return rx
+
+        return rx
