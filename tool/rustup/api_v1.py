88961e486bbd00555ea8c1221a734fff2782058c
Grigori Fursin <gfursin@gmail.com>
2026-04-12 11:52:04 +0200

    fixed bug

--- tool/rustup/api_v1.py
+++ tool/rustup/api_v1.py
@@ -129,7 +129,7 @@ class CTool(InitCTool):
         ###########################################################################################
         # Attempt to run init
 
-        cmd = '{filename} -y --no-modify-path'
+        cmd = f'{filename} -y --no-modify-path'
 
         if 'RUSTUP_HOME' not in env: env['RUSTUP_HOME'] = content_dir
         if 'CARGO_HOME' not in env: env['CARGO_HOME'] = content_dir
