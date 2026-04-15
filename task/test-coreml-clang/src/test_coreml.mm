8df08e254f2b4b1cf688c44115c4100d943216b1
Grigori Fursin <gfursin@gmail.com>
2026-04-15 13:38:26 +0200

    added test-coreml

---
+++ task/test-coreml-clang/src/test_coreml.mm
@@ -0,0 +1,21 @@
+﻿#import <Foundation/Foundation.h>
+#import <CoreML/CoreML.h>
+#include <iostream>
+
+int main() {
+    @autoreleasepool {
+        MLModelConfiguration *cfg = [[MLModelConfiguration alloc] init];
+
+        std::cout << "Core ML framework linked successfully\n";
+
+        if (cfg) {
+            std::cout << "MLModelConfiguration created successfully\n";
+        } else {
+            std::cout << "Failed to create MLModelConfiguration\n";
+            return 1;
+        }
+
+        std::cout << "Done\n";
+    }
+    return 0;
+}
