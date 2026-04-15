169fcb1f8b222e737933194e9bb307fd52490c34
Grigori Fursin <gfursin@gmail.com>
2026-04-15 12:27:56 +0200

    working on metal detection

---
+++ task/target-metal-swift/src/detect-metal.swift
@@ -0,0 +1,34 @@
+﻿import Foundation
+import Metal
+
+func boolDict(_ device: MTLDevice) -> [String: Bool] {
+    var out: [String: Bool] = [:]
+    if #available(macOS 10.15, *) {
+        out["mac2"] = device.supportsFamily(.mac2)
+    }
+    out["mac1"] = device.supportsFamily(.mac1)
+    return out
+}
+
+if let device = MTLCreateSystemDefaultDevice() {
+    let info: [String: Any] = [
+        "available": true,
+        "name": device.name,
+        "isLowPower": device.isLowPower,
+        "isHeadless": device.isHeadless,
+        "hasUnifiedMemory": {
+            if #available(macOS 10.15, *) { return device.hasUnifiedMemory }
+            return false
+        }(),
+        "supportsRaytracing": {
+            if #available(macOS 11.0, *) { return device.supportsRaytracing }
+            return false
+        }(),
+        "families": boolDict(device)
+    ]
+
+    let data = try JSONSerialization.data(withJSONObject: info, options: [.prettyPrinted])
+    print(String(data: data, encoding: .utf8)!)
+} else {
+    print("{\"available\":false}")
+}
