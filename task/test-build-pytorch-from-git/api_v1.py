0a55e30c4a87c47b42601489f0bfde6cee7e5547
Grigori Fursin <gfursin@gmail.com>
2026-04-30 18:20:18 +0200

    clean up

--- task/test-build-pytorch-from-git/api_v1.py
+++ task/test-build-pytorch-from-git/api_v1.py
@@ -95,10 +95,10 @@ class CTask(InitCTask):
             env['OPENSSL_ROOT_DIR'] = ctx_tasks['global']['lib-openssl']['features']['paths']['root']
 
         if 'CMAKE_C_COMPILER' not in env:
-            env['CMAKE_C_COMPILER'] = ctx['global']['host_c_compiler']['path']
+            env['CMAKE_C_COMPILER'] = ctx_tasks['global']['host_c_compiler']['path']
 
         if 'CMAKE_CXX_COMPILER' not in env:
-            env['CMAKE_CXX_COMPILER'] = ctx['global']['host_cpp_compiler']['path']
+            env['CMAKE_CXX_COMPILER'] = ctx_tasks['global']['host_cpp_compiler']['path']
 
         # ROCm
         #If you're compiling for AMD ROCm then first run this command:
